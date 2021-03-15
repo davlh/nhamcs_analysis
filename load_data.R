9c4432dc52f10f098f9a540d2055e049fd0325cc
David Lee-Heidenreich <dleeheid@stanford.edu>
2021-03-14 17:01:20 -0700

    first commit, import stata file data

---
+++ load_data.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(haven)
+path <- "./stata_data/ED2018-stata.dta"
+df <- read_dta(path)
+# TODO:
+# Look into sleep apnea by race and age group 
+# LOok into any papers published about race and sleep apnea
+# Look into what variables the other papers controlled for
+# Look into ANOVA test
+
