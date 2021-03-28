246dc2173bf3fb753cc5bddb34bc78484e3f530b
David Lee-Heidenreich <dleeheid@stanford.edu>
2021-03-27 21:24:37 -0700

    add data, docs, found sleep apnea code

--- load_data.R
+++ load_data.R
@@ -8,3 +8,9 @@ df <- read_dta(path)
 # Look into what variables the other papers controlled for
 # Look into ANOVA test
 
+# ICD-10-CM code for sleep apnea: G47.30
+# do some very basic analysis about sleep apnea diagnosis, ie 
+# what percent of people by race had it?
+# also do some sanity check analysis, maybe for diabetes/heart disease
+
+
